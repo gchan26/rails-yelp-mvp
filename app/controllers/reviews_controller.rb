68b7918cf1b8c0f7d40e11149381d678a76e8c6b
Guilherme Chan <guilhermechan30@gmail.com>
2021-02-17 19:10:25 -0300

    almost done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,48 @@
+class ReviewsController < ApplicationController
+  # def new
+  #   @review = Review.new
+  # end
+
+  # def create
+  #   @review = Review.new(review_params)
+  #   @review.save
+  #   redirect_to review_path(@review)
+  # end
+
+  # private
+
+  # def review_params
+  #   params.require(:review).permit(:rating, :content)
+  # end
+  before_action :find_restaurant, except: [:destroy]
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant)
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
